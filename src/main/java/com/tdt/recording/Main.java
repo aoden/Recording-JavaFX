6235ef4813e414a2be242268e4e25a9c211bf69d
aoden <soap090992@gmail.com>
2015-12-22 07:08:35 +0700

    Initial commit

---
+++ src/main/java/com/tdt/recording/Main.java
@@ -0,0 +1,13 @@
+package com.tdt.recording;
+
+
+import music.QueueRecorder;
+import music.Recorder;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Recorder recorder = new QueueRecorder();
+    }
+}
