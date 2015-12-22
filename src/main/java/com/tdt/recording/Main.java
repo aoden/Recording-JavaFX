ae0e621ef2313b94a8ff639ea9f77a17bdb189ee
aoden <soap090992@gmail.com>
2015-12-22 09:22:56 +0700

    dfd

--- src/main/java/com/tdt/recording/Main.java
+++ src/main/java/com/tdt/recording/Main.java
@@ -1,13 +1,48 @@
 package com.tdt.recording;
 
 
-import music.QueueRecorder;
-import music.Recorder;
+import music.*;
+
+import javax.sound.sampled.AudioFormat;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        //Create all the components
         Recorder recorder = new QueueRecorder();
+        Engine engine = new BasicEngine();
+        AudioInput audioSource = new BasicAudioInput();
+        AudioOutput audioOut = new AudioFileWriter();
+        GUIManager gui = new InterviewGUIManager();
+        NoiseFilter noise = new SimpleNoiseFilter();
+
+        //Configure the components
+        AudioFormat audioInputFormat = new AudioFormat(
+                AudioFormat.Encoding.PCM_SIGNED,
+                44100.0F, 16, 1, 2, 44100.0F, false);
+        AudioFormat audioOutputFormat = new AudioFormat(
+                AudioFormat.Encoding.PCM_SIGNED,
+                44100.0F, 16, 1, 2, 44100.0F, false);
+
+        AudioComponents components = new AudioComponents();
+        components.setAudioChunkSize(1024);
+        components.setAudioInputFormat(audioInputFormat);
+        components.setAudioOutputFormat(audioOutputFormat);
+        components.setRecorder(recorder);
+        components.setAudioInput(audioSource);
+        components.setEngine(engine);
+        components.setGUIManager(gui);
+        components.setAudioOutput(audioOut);
+        components.setNoiseFilter(noise);
+
+        noise.setEnabled(true);
+
+        //Run the components
+
+        components.initialize();
+
+        components.start();
+
     }
 }
